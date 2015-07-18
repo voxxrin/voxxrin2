0c14ea3fbfc1d241b333fb3aab4b7543fecdd3fc
Elian ORIOU <elian.oriou@4sh.fr>
2015-07-19 00:48:06 +0200

    initial commit

---
+++ srv/src/main/java/voxxrin2/AppModule.java
@@ -0,0 +1,74 @@
+package voxxrin2;
+
+import restx.config.ConfigLoader;
+import restx.config.ConfigSupplier;
+import restx.factory.Provides;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableSet;
+import restx.security.*;
+import restx.factory.Module;
+import restx.factory.Provides;
+import javax.inject.Named;
+
+import java.nio.file.Paths;
+
+@Module
+public class AppModule {
+    @Provides
+    public SignatureKey signatureKey() {
+         return new SignatureKey("voxxrin2 voxxrin2 ee144d01-614c-4bc7-a7c4-2a78f507a363 -7912789517386278224".getBytes(Charsets.UTF_8));
+    }
+
+    @Provides
+    @Named("restx.admin.password")
+    public String restxAdminPassword() {
+        return "admin";
+    }
+
+    @Provides
+    public ConfigSupplier appConfigSupplier(ConfigLoader configLoader) {
+        // Load settings.properties in voxxrin2 package as a set of config entries
+        return configLoader.fromResource("voxxrin2/settings");
+    }
+
+    @Provides
+    public CredentialsStrategy credentialsStrategy() {
+        return new BCryptCredentialsStrategy();
+    }
+
+    @Provides
+    public BasicPrincipalAuthenticator basicPrincipalAuthenticator(
+            SecuritySettings securitySettings, CredentialsStrategy credentialsStrategy,
+            @Named("restx.admin.passwordHash") String defaultAdminPasswordHash, ObjectMapper mapper) {
+        return new StdBasicPrincipalAuthenticator(new StdUserService<>(
+                // use file based users repository.
+                // Developer's note: prefer another storage mechanism for your users if you need real user management
+                // and better perf
+                new FileBasedUserRepository<>(
+                        StdUser.class, // this is the class for the User objects, that you can get in your app code
+                        // with RestxSession.current().getPrincipal().get()
+                        // it can be a custom user class, it just need to be json deserializable
+                        mapper,
+
+                        // this is the default restx admin, useful to access the restx admin console.
+                        // if one user with restx-admin role is defined in the repository, this default user won't be
+                        // available anymore
+                        new StdUser("admin", ImmutableSet.<String>of("*")),
+
+                        // the path where users are stored
+                        Paths.get("data/users.json"),
+
+                        // the path where credentials are stored. isolating both is a good practice in terms of security
+                        // it is strongly recommended to follow this approach even if you use your own repository
+                        Paths.get("data/credentials.json"),
+
+                        // tells that we want to reload the files dynamically if they are touched.
+                        // this has a performance impact, if you know your users / credentials never change without a
+                        // restart you can disable this to get better perfs
+                        true),
+                credentialsStrategy, defaultAdminPasswordHash),
+                securitySettings);
+    }
+}
