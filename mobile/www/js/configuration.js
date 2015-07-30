34f84193228a5e0a594e4faced50fda4c4b7a4a1
Elian ORIOU <elian.oriou@4sh.fr>
2015-07-30 19:07:11 +0200

    add some comments on configuration constant

--- mobile/www/js/configuration.js
+++ mobile/www/js/configuration.js
@@ -3,6 +3,12 @@
 angular.module('voxxrin')
     .constant('configuration', {
 
-        backendUrl: 'http://192.168.0.15:8080'
+        /////////////////////////////////////////
+        // This configuration must be filled
+        /////////////////////////////////////////
+
+        // The URL of the RESTx backend
+        // backendUrl: 'http://192.168.0.15:8080'
+
 
     });
