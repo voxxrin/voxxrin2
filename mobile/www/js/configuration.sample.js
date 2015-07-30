a58a997b2784a089d76e5f3b9d0e5a19dbc03cb3
Elian ORIOU <elian.oriou@4sh.fr>
2015-07-30 19:09:56 +0200

    add sample mobile configuration

---
+++ mobile/www/js/configuration.sample.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/////////////////////////////////////////
+// This configuration must be filled
+/////////////////////////////////////////
+
+//angular.module('voxxrin')
+//    .constant('configuration', {
+
+        // The URL of the RESTx backend
+        // backendUrl: 'http://192.168.0.15:8080'
+
+    //});
