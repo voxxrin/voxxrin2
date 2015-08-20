88d2e1c5ed3a14cfeab003b3059d37ca3b1b2ab0
Elian ORIOU <elian.oriou@4sh.fr>
2015-08-20 22:17:35 +0200

    mock days tabs

--- mobile/www/js/controllers/PresentationsListCtrl.js
+++ mobile/www/js/controllers/PresentationsListCtrl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('voxxrin')
-    .controller('PresentationsListCtrl', function ($stateParams, $scope) {
+    .controller('PresentationsListCtrl', function ($stateParams, $scope, Day) {
 
+        $scope.days = Day.fromEvent($stateParams.eventId);
     });
