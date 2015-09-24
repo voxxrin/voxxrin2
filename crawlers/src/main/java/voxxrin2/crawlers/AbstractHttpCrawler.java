900d464be65533112c9ad40beb8c11ddc8580c11
Elian ORIOU <elian.oriou@4sh.fr>
2015-09-24 18:23:44 +0200

    Add base voxxrin 1 crawler + add small test

--- crawlers/src/main/java/voxxrin2/crawlers/AbstractHttpCrawler.java
+++ crawlers/src/main/java/voxxrin2/crawlers/AbstractHttpCrawler.java
@@ -3,6 +3,7 @@ package voxxrin2.crawlers;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,6 +20,7 @@ public abstract class AbstractHttpCrawler {
 
     protected void configureMapper(ObjectMapper mapper) {
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(new JodaModule());
     }
 
     protected CollectionType buildCollectionType(Class<?> clazz) {
