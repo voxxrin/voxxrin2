7bfd607cb481d230ad1597aca887770a2627826e
Elian ORIOU <elian.oriou@4sh.fr>
2015-10-11 14:57:43 +0200

    disable calendar if the mobile app is not executed into a cordova env

--- mobile/www/js/services/Calendar.js
+++ mobile/www/js/services/Calendar.js
@@ -3,20 +3,27 @@
 angular.module('voxxrin')
     .service('Calendar', function ($cordovaCalendar) {
 
+        var _createEntry = function (presentation, event) {
+
+            $cordovaCalendar.createEventInteractively({
+                title: '[ ' + event.name + ' ] ' + presentation.title,
+                location: presentation.location ? presentation.location.fullName : '-',
+                startDate: moment(presentation.from).toDate(),
+                endDate: moment(presentation.to).toDate()
+            }).then(function () {
+                console.log('L\'evenement a été ajouté à votre calendrier !');
+            }, function (err) {
+                console.log('Error during event creation.', err);
+            });
+        };
+
         return {
             createEntry: function (presentation, event) {
-
-                $cordovaCalendar.createEventInteractively({
-                    title: '[ ' + event.name + ' ] ' + presentation.title,
-                    location: presentation.location ? presentation.location.fullName : '-',
-                    startDate: moment(presentation.from).toDate(),
-                    endDate: moment(presentation.to).toDate()
-                }).then(function () {
-                    console.log('L\'evenement a été ajouté à votre calendrier !');
-                }, function (err) {
-                    console.log('Error during event creation.', err);
-                });
-
+                if (window.cordova) {
+                    _createEntry(presentation, event);
+                } else {
+                    console.log('No calendar is supported on this device');
+                }
             }
         };
     });
