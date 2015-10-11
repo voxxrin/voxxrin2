1b358df95739b2dff377258247cdbd899e654dac
Elian ORIOU <elian.oriou@4sh.fr>
2015-10-12 00:16:42 +0200

    improve favorites extract script

--- data/scripts/___extract_favorites.js
+++ data/scripts/___extract_favorites.js
@@ -4,5 +4,5 @@ db.favorite.find().forEach(function (_favorite) {
     var user = db.users.findOne({_id: ObjectId(_favorite.userId)});
     var prez = resolveRef(_favorite.presentation).get();
     var event = resolveRef(prez.event).get();
-    print(user.login + ' -> ' + prez.title + ' (' + event.name + ')');
+    print(user._id + '-' + user.login + ' -> ' + prez.title + ' (' + event.name + ')' + ' from ' + (_favorite.source ? _favorite.source.from : 'voxxrin2'));
 });
