7f5a2b47c56ed87d1b39cf56c24b0ee0322b8534
Elian ORIOU <elian.oriou@4sh.fr>
2016-01-03 16:44:49 +0100

    add a resource allowing to consult registered crawlers

--- crawlers/src/main/java/crawlers/web/rest/CrawlersResource.java
+++ crawlers/src/main/java/crawlers/web/rest/CrawlersResource.java
@@ -1,11 +1,24 @@
 package crawlers.web.rest;
 
+import crawlers.AbstractHttpCrawler;
+import restx.annotations.GET;
 import restx.annotations.RestxResource;
+import restx.factory.Component;
+
+import java.util.Set;
 
 @RestxResource
+@Component
 public class CrawlersResource {
 
-    public CrawlersResource() {
+    private final Set<AbstractHttpCrawler> crawlers;
+
+    public CrawlersResource(Set<AbstractHttpCrawler> crawlers) {
+        this.crawlers = crawlers;
     }
 
+    @GET("/info")
+    public Iterable<AbstractHttpCrawler> getAllRegisteredCrawlers() {
+        return crawlers;
+    }
 }
