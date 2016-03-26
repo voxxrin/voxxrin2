a7ef7aeecb06b5bb0fbdeb95d48b00b6a569f981
Elian ORIOU <elian.oriou@4sh.fr>
2016-03-26 22:40:08 +0100

    update android + ios release scripts

--- mobile/androidRelease.sh
+++ mobile/androidRelease.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Avez-vous verifié la configuration backend sur le frontend ?"
+read yes
+
 PATH=$(npm bin):$PATH
 cordova build --release android
 echo "Path of the android keystore ?"
