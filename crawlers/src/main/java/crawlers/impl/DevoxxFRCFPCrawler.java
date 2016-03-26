abb48feb2c1e7fe91d18711d95032ccb948dcb28
Elian ORIOU <elian.oriou@4sh.fr>
2016-03-26 22:08:26 +0100

    improve devoxx fr cfp crawler

--- crawlers/src/main/java/crawlers/impl/DevoxxFRCFPCrawler.java
+++ crawlers/src/main/java/crawlers/impl/DevoxxFRCFPCrawler.java
@@ -1,7 +1,13 @@
 package crawlers.impl;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import crawlers.CrawlingResult;
+import org.bson.types.ObjectId;
 import restx.factory.Component;
+import voxxrin2.domain.Room;
 
 @Component
 public class DevoxxFRCFPCrawler extends DevoxxCFPCrawler {
@@ -10,5 +16,35 @@ public class DevoxxFRCFPCrawler extends DevoxxCFPCrawler {
 
     public DevoxxFRCFPCrawler() {
         super("devoxxfr", ImmutableList.of("devoxxfr-publisher"), BASE_URL);
+    }
+
+    @Override
+    protected void crawlDay(CrawlingResult result, DevoxxCFPCrawler.CFPLink dayLink, DevoxxCFPCrawler.CFPDay cfpDay) {
+        populateRooms(result, cfpDay);
+        super.crawlDay(result, dayLink, cfpDay);
+    }
+
+    /**
+     * Rooms are not ALWAYS properly provided by the DevoxxFR CFP API, so we need to build a rooms list based
+     * on roomName attributes found into a slot
+     */
+    private void populateRooms(CrawlingResult result, CFPDay cfpDay) {
+
+        for (final CFPSlot slot : cfpDay.slots) {
+            Optional<Room> registeredRoom = Iterables.tryFind(result.getRooms(), new Predicate<Room>() {
+                @Override
+                public boolean apply(Room input) {
+                    return input.getName().equals(slot.roomId);
+                }
+            });
+            if (!registeredRoom.isPresent()) {
+                result.getRooms().add(
+                        (Room) new Room()
+                                .setFullName(slot.roomName)
+                                .setName(slot.roomId)
+                                .setKey(new ObjectId().toString())
+                );
+            }
+        }
     }
 }
