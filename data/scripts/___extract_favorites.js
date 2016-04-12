b0a57ebc87715a23a5ee80be146d0e76431793d4
Elian ORIOU <elian.oriou@4sh.fr>
2016-04-13 00:21:06 +0200

    remove data folder

--- data/scripts/___extract_favorites.js
+++
@@ -1,8 +0,0 @@
-db.loadServerScripts();
-
-db.favorite.find().forEach(function (_favorite) {
-    var user = db.users.findOne({_id: ObjectId(_favorite.userId)});
-    var prez = resolveRef(_favorite.presentation).get();
-    var event = resolveRef(prez.event).get();
-    print(user._id + '-' + user.login + ' -> ' + prez.title + ' (' + event.name + ')' + ' from ' + (_favorite.source ? _favorite.source.from : 'voxxrin2'));
-});
