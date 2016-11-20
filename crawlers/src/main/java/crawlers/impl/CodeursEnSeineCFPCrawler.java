1ce8be176f1f9266e1dae64d6b1c4b47c45d4e26
Elian ORIOU <elian.oriou@4sh.fr>
2016-11-20 14:57:31 +0100

    change ces event crawler type (DevoxxCFP -> Lanyrd)

--- crawlers/src/main/java/crawlers/impl/CodeursEnSeineCFPCrawler.java
+++ crawlers/src/main/java/crawlers/impl/CodeursEnSeineCFPCrawler.java
@@ -4,11 +4,9 @@ import com.google.common.collect.ImmutableList;
 import restx.factory.Component;
 
 @Component
-public class CodeursEnSeineCFPCrawler extends DevoxxCFPCrawler {
-
-    private static final String BASE_URL = "http://cfp.codeursenseine.com/api/conferences/";
+public class CodeursEnSeineCFPCrawler extends LanyrdCrawler {
 
     public CodeursEnSeineCFPCrawler() {
-        super("ces", ImmutableList.of("ces-publisher"), BASE_URL);
+        super("ces", ImmutableList.of("ces-publisher"));
     }
 }
